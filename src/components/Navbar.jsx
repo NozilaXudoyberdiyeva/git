bcfcd08a328914359ee84a90a0fc3d8c27473af2
Your Name <you@example.com>
2025-10-20 19:08:44 +0500

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Logo</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contacts</li>
+        <li>Location</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
