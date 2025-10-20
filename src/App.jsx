bf3cc0250b7df9b6e5a1167a80ed01342293f790
abdulaziz <rixsiboeauabdulaziz@gmail.com>
2025-10-20 19:36:20 +0500

    about don

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
+import About from './pages/About'
 
 const App = () => {
   return (
     <div>
-    <Routes>
-    
-      <Route path='/' element={<Layout />}>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='about' element={<About />} />
+        </Route>
+      </Routes>
 
-      </Route>
-    </Routes>
-      
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div>
       <h1>Logo</h1>
       <ul>
         <li>Home</li>
+        <Link to="about">
         <li>About</li>
+        </Link>
         <li>Contacts</li>
         <li>Location</li>
         <li>Login</li>
